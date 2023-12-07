1f35ad1b61fddf687ba2c26d09a9fb3fc9d917ea
TrenEnjoyerXX1 <153177440+TrenEnjoyerXX1@users.noreply.github.com>
2023-12-08 00:52:08 +0200

    changed html to php and applied a static navbar for all current pages

---
+++ Pages/CustomerWax.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>AutoCar Masters | Wax</title>
+
+  
+
+  
+</head>
+<body>
+
+    <div class="navbar navbar-expand-lg bg-body-tertiary">
+      <?php include 'navbar.html'; ?>    
+    </div>
+
+
+
+
+</body>
+</html>
---
+++ Pages/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AutoCar Masters | Home</title>
+    
+  </head>
+  <body>
+  
+    <div class="navbar navbar-expand-lg bg-body-tertiary">
+      <?php include 'navbar.html'; ?>
+    </div>
+
+
+
+
+
+
+  </body>
+</html>
