8cfc013d3c4b73727559515aceb86a51017341b6
TrenEnjoyerXX1 <153177440+TrenEnjoyerXX1@users.noreply.github.com>
2023-12-08 17:31:46 +0200

    test phpstorm committing.

--- PHP/testhash.php
+++ PHP/testhash.php
@@ -7,9 +7,12 @@ $password = 'your_password_here';
 // Hash the password
 echo password_hash($password, CRYPT_SHA512);
 
+<<<<<<< HEAD
 echo "test";
 
 
+=======
+>>>>>>> 734fd36 (test phpstorm committing.)
 
 
 
