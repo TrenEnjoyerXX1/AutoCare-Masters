8b24ca1d7f92e681df8d08c079c1647d0f1fc385
TrenEnjoyerXX1 <153177440+TrenEnjoyerXX1@users.noreply.github.com>
2023-12-23 16:37:45 +0200

    adminPage

---
+++ PHP/Admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AutoCare Masters | Admin</title>
+
+
+</head>
+<body>
+
+<?php include 'navbar.php'; ?>
+
+
+
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
