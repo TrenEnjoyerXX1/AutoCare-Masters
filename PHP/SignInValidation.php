3c008be038b7735c54be6648fb4eed1bc3b06e0b
TrenEnjoyerXX1 <153177440+TrenEnjoyerXX1@users.noreply.github.com>
2023-12-26 23:23:08 +0200

    lololololol

--- PHP/SignInValidation.php
+++ PHP/SignInValidation.php
@@ -1,9 +1,11 @@
 <?php
+global $con;
 include("DataBaseConfig.php");
 
 $Username=null;
 $password=null;
 $Role=null;
+$stmt=null;
 
 if(isset($_POST["signinbutton"]))
 {
@@ -14,105 +16,113 @@ if(isset($_POST["signinbutton"]))
 
 if (isset($_POST['StaffFlag']))
 {
-    // Checkbox was checked--> Staff User
+    session_start();
+        // Checkbox was checked--> Staff User
 
-    $sql= "SELECT   `Role`  FROM `staff` WHERE `UserName`= ? AND `Password`= ? ;" ;
-    $stmt->bind_param("ss",$Username,$password);
-    $stmt->execute();
-    $res=$stmt->get_result();
+        if($Username==="admin" && $password===hash('sha3-512', "admin"))
+        {//admin
 
+            $_SESSION['name'] = "Admin";
 
-    $Signin=mysqli_query($con,$sql);
-
-    if($Username==="admin"&&$password==="5a38afb1a18d408e6cd367f9db91e2ab9bce834cdad3da24183cc174956c20ce35dd39c2bd36aae907111ae3d6ada353f7697a5f1a8fc567aae9e4ca41a9d19d")
-    {//admin
-        header('Location:Admin.php');
-        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
-    }
-    else if($res= mysqli_fetch_array($Signin))
-    {
-        if($res['Role'])// byshof eh ele fe result ele rag3a mn query
+            header('Location:Admin.php');
+            exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+        }
+        else if($Username!=="admin")// not admin
         {
-          $Role=$res['Role'];
+            $sql= "SELECT   `Role`,`F_Name`,`S_D_No`  FROM `staff` WHERE `UserName`= ? AND `Password`= ? ; ";
+                    $stmt->$con->prepare($sql);
+                    $stmt->bind_param("ss",$Username,$password);
+                    $stmt->execute();
+                    $result=$stmt->get_result();
 
-              if($Role==="wash")
-              {//wash
+            if ($result->num_rows>0)
+            {
+                $StaffData=$result->fetch_assoc();
 
-                  header('Location:ShopWash.php');
-                  exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+                $_SESSION['name']=$StaffData['F_name'];
+                $_SESSION['role']=$StaffData['Role'];
+                $_SESSION['S_D_No']=$StaffData['S_D_No'];
 
-              }
-              else if($Role==="repair")
-              {//repair
-                  header('Location:ShopRepair.php');
-                  exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+                $Role=$_SESSION['role'];
 
-              }
-              else if($Role==="wax")
-              {//wax
-                  header('Location:ShopWax.php');
-                  exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+                    if($Role==="wash")
+                    {//wash
 
-              }
-              else if($Role==="wrap")
-              {//wrap
-                  header('Location:ShopCarWrap.php');
-                  exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+                        header('Location:ShopWash.php');
+                        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
 
-              }
-              else if($Role==="tuning")
-              {//tuning
+                    }
+                    else if($Role==="repair")
+                    {//repair
+                        header('Location:ShopRepair.php');
+                        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
 
-                  header('Location:ShopTuning.php');
-                  exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+                    }
+                    else if($Role==="wax")
+                    {//wax
+                        header('Location:ShopWax.php');
+                        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
 
+                    }
+                    else if($Role==="wrap")
+                    {//wrap
+                        header('Location:ShopCarWrap.php');
+                        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
 
-              }
-              else if($Role==="upgrade")
-              {//upgrade
-                  header('Location:ShopUpgrade.php');
-                  exit;// (Add-on) to prevent accessing the rest of this page after redirecting
+                    }
+                    else if($Role==="tuning")
+                    {//tuning
 
-              }
-
-        }
-        else
-        {//user isn't in the Database or not a staff member
+                        header('Location:ShopTuning.php');
+                        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
 
 
+                    }
+                    else if($Role==="upgrade")
+                    {//upgrade
+                        header('Location:ShopUpgrade.php');
+                        exit;// (Add-on) to prevent accessing the rest of this page after redirecting
 
-        }
+                    }
 
+                }
+                else
+                {//user isn't in the Database or not a staff member
+
+                    include_once "SignIn.php";
+                    echo "Incorrect username or password.";
+                }
     }
     else
     {
-      echo "else ya bro";
+      echo "لا انت ادمن ولا انت موظف, هزر بعيد بالله عليك مش هنا السيرفر هيموت من الضحك كده";
     }
 }
 else
 {
     // Checkbox was not checked--> Customer
 
-        $sql= "SELECT   count(*)  FROM `customer` WHERE `UserName`=" . $Username . "  AND `Password`=" .$password . ";" ;
+        $sql= "SELECT   `F_Name`  FROM `customer` WHERE `UserName`=?  AND `Password`=? ;" ;
 
-        $Signin=mysqli_query($con,$sql);
+    $stmt->$con->prepare($sql);
+    $stmt->bind_param("ss",$Username,$password);
+    $stmt->execute();
+    $result=$stmt->get_result();
 
-         if($res= mysqli_fetch_array($Signin))
-        {
-            if($res['count(*)']===1)// byshof eh ele fe result ele rag3a mn query
-            {
-                //
-                echo "signed in successfully";
+            if ($result->num_rows>0)
+            {//fe row ya3ne customer bgd
+                $CustomerData=$result->fetch_assoc();
 
-                //set session values
+                $_SESSION['name']=$CustomerData['F_name'];
+
             }
-        }
+            else
+            {//user isn't in the Database
 
-
+                include_once "SignIn.php";
+                echo "Incorrect username or password.";
+            }
 
 }
-
-
-
 
 ?>
